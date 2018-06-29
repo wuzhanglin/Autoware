9f9f6aaeb03d2020591e3dbf59f6e2b1cfe09430
David Uhm <david.uhm@lge.com>
2018-06-28 17:04:44 -0700

    Fix bug in way planner

--- ros/src/computing/planning/mission/packages/way_planner/nodes/way_planner_core.cpp
+++ ros/src/computing/planning/mission/packages/way_planner/nodes/way_planner_core.cpp
@@ -385,7 +385,8 @@ bool way_planner_core::GenerateGlobalPlan(PlannerHNS::WayPoint& startPoint, Plan
 
 	std::vector<int> predefinedLanesIds;
 	double ret = m_PlannerH.PlanUsingDP(startPoint, goalPoint,
-			MAX_GLOBAL_PLAN_DISTANCE, predefinedLanesIds,
+			MAX_GLOBAL_PLAN_DISTANCE, m_params.bEnableLaneChange,
+			predefinedLanesIds,
 			m_Map, generatedTotalPaths, &m_PlanningVisualizeTree);
 
 	m_pCurrGoal = PlannerHNS::MappingHelpers::GetClosestWaypointFromMap(goalPoint, m_Map);
